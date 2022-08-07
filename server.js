5c37e3775f1c2a8c6fec292f9d853500b9e1b0c7
President <aaron.m.wilson@uconn.edu>
2022-08-06 20:24:38 -0400

    created server.js file and uploaded npm packages for express and mysql2

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mysql = require("mysql2");
+
+const port = process.env.PORT || 3001;
+const app = express();
+
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+
+        user: "root",
+    }
+)
